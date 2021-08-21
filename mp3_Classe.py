bdedcf5b66d0383ae02129feb9c14f5c57745786
David pequeno Marinho <63377354+David-Marinho@users.noreply.github.com>
2021-08-21 13:03:55 -0300

    update

--- mp3_Classe.py
+++ mp3_Classe.py
@@ -1,50 +1,31 @@
 import tkinter as tk
+from func import tocar
+from func_menu import *
 
 
 class mp3:
     def __init__(self, root: tk.Tk):
         self.root = root
-        """self.music_Btn = music_Btn
-        self.menu_Btn = menu_Btn
-        self.menu_Label = menu_Label"""
 
-    def criar_Menu(self):
-        global menu_Img
-        menu_Img = tk.PhotoImage(file='imagens/barra.png')
-        menu = tk.Label(self.root, image=menu_Img, borderwidth=0)
-        menu.place(x=0, y=410)
-        self.btn_PausePlay()
 
 
     def algo(self):
         pass
 
-    def btn_PausePlay(self):
-        global btn_Img
-        btn_Img = tk.PhotoImage(file='imagens/botao pause.png')
-        btn = tk.Button(self.root, image=btn_Img, borderwidth=0, command=self.algo, bg='#444444')
-        btn.place(x=390, y=450)
-        self.criar_prox()
 
+    def criar_musica(self, musicas):
+        global barra, btn
+        global i
+        eixoY = 30
+        barra = tk.PhotoImage(file='imagens/caixas.png')
+        for i in range(len(musicas)):
+            btn = tk.Button(self.root, image=barra, borderwidth=0, text=musicas[i], bg='#444444')
+            btn.place(x=20, y=eixoY)
+            eixoY += 90
+            btn.bind("<Button-1>", self.escolher_musica)
 
-    def criar_prox(self):
-        global prox_Img
-        prox_Img = tk.PhotoImage(file='imagens/botao skip.png')
-        btn = tk.Button(self.root, image=prox_Img, borderwidth=0, command=self.algo, bg='#444444')
-        btn.place(x=500, y=450)
-        self.criar_back()
+    def lb_musica(self, texto, eixoY):
+        tk.Label().place(x=30, y=eixoY + 20)
 
-    def criar_back(self):
-        global back_Img
-        back_Img = tk.PhotoImage(file='imagens/botao back.png')
-        btn = tk.Button(self.root, image=back_Img, borderwidth=0, command=self.algo, bg='#444444')
-        btn.place(x=280, y=450)
-        self.criar_nome()
-
-
-
-    def criar_nome(self):
-        lb = tk.Label(self.root, text='Só um simples teste', bg='#444444')
-        lb.configure(font=("Arial", 16))
-        lb.place(x=310, y=415)
-
+    def escolher_musica(self, event=None):
+        tocar(event.widget['text'])
