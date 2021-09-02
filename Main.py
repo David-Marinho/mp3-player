baccdbb03831740d00aed595d264af589a5b6b76
David pequeno Marinho <63377354+David-Marinho@users.noreply.github.com>
2021-09-01 22:41:35 -0300

    ajustar resoluçao

--- Main.py
+++ Main.py
@@ -2,10 +2,13 @@ from tkinter import *
 from musicas_Classe import bt_musicas
 from os import listdir
 from classe_menu import menu
+from win32api import GetSystemMetrics
 
+largura = GetSystemMetrics(0)
+altura = GetSystemMetrics(1)
 arq = listdir('musicas')
 raiz = Tk()
-raiz.geometry('850x500')
+raiz.geometry(f'{largura}x{altura}')
 
 img = PhotoImage(file='imagens/caixas.png')
 for i in range(len(arq)):
--- classe_menu.py
+++ classe_menu.py
@@ -1,13 +1,12 @@
 from func import *
 
+
 class menu():
     def __init__(self, raiz):
         self.raiz = raiz
 
     def criar_menu(self):
-        global barra_img
-        barra_img = PhotoImage(file='imagens/barra.png')
-        Label(self.raiz, image=barra_img, borderwidth=0).place(x=0, y=410)
+        Label(self.raiz, bg='#65B5FF', borderwidth=0, height=8, width=122).place(x=0, y=390)
         self.criar_botoes()
 
     def criar_botoes(self):
@@ -15,5 +14,5 @@ class menu():
 
         image1 = PhotoImage(file='imagens/botao pause.png')
 
-        pausePlay = Button(self.raiz, image=image1, borderwidth=0, bg='#444444', command=lambda: tocando(pausePlay))
-        pausePlay.place(x=390, y=450)
+        pausePlay = Button(self.raiz, image=image1, borderwidth=0, bg='#65B5FF', command=lambda: tocando(pausePlay))
+        pausePlay.place(x=390, y=400)
